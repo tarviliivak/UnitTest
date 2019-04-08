2b7e5bca990eb643b5cf8b09cf81b0db0e270478
T_Liivak <tarvi.liivak@khk.ee>
2019-04-08 10:16:52 +0300

    1. Write a unit test(updated comments)

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -8,13 +8,15 @@ use TDD\Receipt;
 class ReceiptTest extends TestCase {
     public function testTotal() {
         $Receipt = new Receipt();
+        //create a new instance of the Receipt class
         $this->assertEquals(
+        //write first assertion to pass in a simple array of some ints
             15,
-            //expectec  value
+        //we'll set the expected value of 15
             $Receipt->total([0,2,5,8]),
-            //actual value
+        //we'll pass in our variable Receipt->totaland pass in an array with the values zero, two, five, and eight on line 16
+
             'When summing the total should equal 15'
         );
     }
 }
-//This test expects that the Receipt total sum will be 15
