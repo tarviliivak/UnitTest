325513b41acb7de14dc057ea674848f013949463
T_Liivak <tarvi.liivak@khk.ee>
2019-04-08 10:37:08 +0300

    2. Extend  a unit test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -6,16 +6,29 @@ use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
-    public function testTotal() {
-        $Receipt = new Receipt();
+    public function setUp() {
+        //we'll add public function setUpand
+        $this->Receipt = new Receipt();
+        //we'll add this arrow Receipt is equal to new Receiptinside of the setUp method
         //create a new instance of the Receipt class
+    }
+
+    public function tearDown() {
+    //we'll add public function tearDown
+        unset($this->Receipt);
+    // we'll add unset this arrow Receipt
+    }
+    public function testTotal() {
+        $input = [0,2,5,8];
+        $output = $this->Receipt->total($input);
+        //we'll add variable output is equal to this arrow Receipt arrow total, pass in our variable input, add our semicolon and we'll take output and set it on line 21
+
         $this->assertEquals(
         //write first assertion to pass in a simple array of some ints
             15,
-        //we'll set the expected value of 15
-            $Receipt->total([0,2,5,8]),
-        //we'll pass in our variable Receipt->totaland pass in an array with the values zero, two, five, and eight on line 16
-
+            //we will erase the Receipt arrow total
+            //we'll set the expected value of 15
+            $output,
             'When summing the total should equal 15'
         );
     }
